5a22abeb56c4495a40304f27ae4e07a81f1181b0
Harold Campbell <harold.campbell@gmail.com>
2018-05-31 08:10:51 +0200

    [chore] code cleanup

--- experimental/src/experiment2.js
+++ experimental/src/experiment2.js
@@ -19,7 +19,9 @@ import {
   let barsData = $data([10, 20, 30, 40, 50, 40, 30, 20, 10, 50, 60, 10, 30, 80], "height");
   barsData.withVisibleItems(5)
 
-  container("c-experiment-2", _ => {
+  let barsData2 = $data([10, 20, 30, 40, 50, 40, 30, 20, 10, 50, 60, 10, 30, 80], "height");
+
+  container("w-experiment-2", _ => {
     bars1 = _.empty()
     .withData(barsData)
     .withIntents([$maxHeight(50), $alignBottom(100), $width(15), $xOffset(20)])
@@ -29,7 +31,11 @@ import {
         .$y(0)
         .$class("bar");
     });
-  }, $id("experiment-2"));
+  }, $id("window-experiment-2"));
+
+  container("u-experiment-2", _ => {
+    _.bars(barsData2, [$maxHeight(50), $alignBottom(100), $width(15), $xOffset(20)]);
+  }, $id("unwindowed-experiment-2"));
 
 
   window.onload = function () {
@@ -38,8 +44,7 @@ import {
     slider.knob.onRangeChanged = function (s) {
       let index = Math.floor(s.knob.range * barsData.itemCount());
       barsData.withVisibleItems(5, index);
-      // barsData.DataDidChange();
-      // bars1.onDataDidChange();
+      barsData
 
       let labelElm = document.getElementById('slider-text-2');
       labelElm.innerText = `index: ${index}`;
--- test/unit/intents-data.spec.js
+++ test/unit/intents-data.spec.js
@@ -1,14 +1,80 @@
-import {$data} from "../../lib/ancui-data"
+import {
+  $data
+} from "../../lib/ancui-data"
 
-describe("Data Intents", () => {
-  it("max() should return max value", () => {
+describe("Data max()", () => {
+  it("should return max value", () => {
     let data = $data([10, 15, 5, 20]);
 
     expect(data.max()).toBe(20);
   });
 });
 
-describe("Data Intents", () => {
+describe("Data appendDataStart()", () => {
+  let data;
+
+  beforeEach(() => {
+    data = $data([10, 20]);
+  });
+
+  it("should to tail of the data", () => {
+    let actual = data.activeDataItems()
+
+    expect(actual[0]).toBe(0.5)
+    expect(actual[1]).toBe(1)
+
+    data.appendDataStart([15])
+    actual = data.activeDataItems()
+
+    expect(actual[0]).toBe(0.5)
+    expect(actual[1]).toBe(1)
+    expect(actual[2]).toBe(0.75)
+  });
+
+  it("should re-normalize data when it contains bigger values", () => {
+    data.appendDataStart([40])
+
+    let actual = data.activeDataItems()
+
+    expect(actual[0]).toBe(0.25)
+    expect(actual[1]).toBe(0.5)
+    expect(actual[2]).toBe(1)
+  });
+});
+
+describe("Data appendDataEnd()", () => {
+  let data;
+
+  beforeEach(() => {
+    data = $data([10, 20]);
+  });
+
+  it("should appendDataEnd() to head of the data", () => {
+    let actual = data.activeDataItems()
+
+    expect(actual[0]).toBe(0.5)
+    expect(actual[1]).toBe(1)
+
+    data.appendDataEnd([15])
+    actual = data.activeDataItems()
+
+    expect(actual[0]).toBe(0.75)
+    expect(actual[1]).toBe(0.5)
+    expect(actual[2]).toBe(1)
+  });
+
+  it("should re-normalize data when appendDataEnd() contains bigger values", () => {
+    data.appendDataEnd([40])
+
+    let actual = data.activeDataItems()
+
+    expect(actual[0]).toBe(1)
+    expect(actual[1]).toBe(0.25)
+    expect(actual[2]).toBe(0.5)
+  });
+});
+
+describe("Data", () => {
   let data;
 
   beforeEach(() => {
@@ -25,7 +91,7 @@ describe("Data Intents", () => {
   it("should set activeDataItems() to _asClipped() when clipped", () => {
     // By setting visibleItems to less than the
     // data.itemCount() we are clipping the data
-    const numVisibleItems = data.itemCount()/2
+    const numVisibleItems = data.itemCount() / 2
 
     data.withVisibleItems(numVisibleItems)
     const actual = data.activeDataItems()
