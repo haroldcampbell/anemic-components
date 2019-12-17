9793f267a01ca86219bda7f56dde64ad3d6e660e
haroldcampbell <harold.campbell@gmail.com>
2019-12-17 14:15:24 +0200

    Update segment visual examples

--- www/examples/src/examples-segment.js
+++ www/examples/src/examples-segment.js
@@ -8,36 +8,213 @@ import {
     $arcRotateBy,
     $arcSpanUnit,
     $arcSpanOffset,
-    $arcLambda,
     container,
     segments,
 
-    ellipses,
-    $xOffset,
-    $max,
-    arcs,
     $radialOffset,
     $arcSpread,
+    $arcSpanInset,
+    $group,
 } from './../../assets/dist/ancui.js'
-
-let cirData = $data([10, 10, 10, 10, 10]);
 
 let exSegmentData1 = $data([45, 45, 90, 180]);
 
-// total =
 container("segment-1", _ => {
-    _.addVisuals([
-        segments(exSegmentData1, [$x(100), $y(100), $arcSpread(360), $arcSpanOffset(0), $arcRadius(50)]),
-    ]);
-}, $id("example-segment-1"));
+    const [x1, x2, x3, x4] = [60, 180, 300, 425];
+    const [y1, y2, y3] = [80, 190, 325];
 
-container("segment-2", _ => {
     _.addVisuals([
-        ellipses(cirData, [$y(100), $x(100), $xOffset(50), $max(10)]),
+        // ROW 1
         segments(exSegmentData1, [
-            $x(100),
-            $y(100), $arcSpanUnit(60), $arcSpanOffset(10), $arcRadius(50),
-            $radialOffset(10, 3)
+            $group("y1-x1"),
+            $x(x1), $y(y1),
+            $arcRadius(40), $arcSpanUnit(90),
+        ]),
+
+        segments(exSegmentData1, [
+            $group("y1-x2"),
+            $x(x2), $y(y1),
+            $arcRadius(40), $arcSpanUnit(90), $arcRadiusOffset(5),
+        ]),
+
+        segments(exSegmentData1, [
+            $group("y1-x3"),
+            $x(x3), $y(y1),
+            $arcRadius(40), $arcSpanUnit(90), $arcRadiusOffset(5), $arcRotateBy(30),
+        ]),
+
+        segments(exSegmentData1, [
+            $group("y1-x4"),
+            $x(x4), $y(y1),
+            $arcRadius(40), $arcSpanUnit(90), $arcRadiusOffset(5), $radialOffset(20, 2),
+        ]),
+
+        // ROW 2
+        segments(exSegmentData1, [
+            $group("y2-x1"),
+            $x(x1), $y(y2),
+            $arcRadius(40), $arcSpanUnit(90), $arcSpanOffset(0),
+        ]),
+
+        segments(exSegmentData1, [
+            $group("y2-x2"),
+            $x(x2), $y(y2),
+            $arcRadius(40), $arcSpanUnit(90), $arcSpanOffset(10),
+        ]),
+
+        segments(exSegmentData1, [
+            $group("y2-x3"),
+            $x(x3), $y(y2),
+            $arcRadius(40), $arcSpanUnit(90), $arcSpanOffset(10), $arcRotateBy(30),
+        ]),
+
+        segments(exSegmentData1, [
+            $group("y2-x4"),
+            $x(x4), $y(y2),
+            $arcRadius(40), $arcSpanUnit(90), $arcSpanOffset(10), $arcRotateBy(30), $radialOffset(10, 2),
+        ]),
+
+        // ROW 3
+        segments(exSegmentData1, [
+            $group("y3-x1"),
+            $x(x1), $y(y3),
+            $arcSpread(360), $arcRadius(40),
+        ]),
+
+        segments(exSegmentData1, [
+            $group("y3-x2"),
+            $x(x2), $y(y3),
+            $arcSpread(360), $arcRadius(40), $arcSpanInset(5),
+        ]),
+
+        segments(exSegmentData1, [
+            $group("y3-x3"),
+            $x(x3), $y(y3),
+            $arcSpread(360), $arcRadius(40), $arcSpanInset(5), $radialOffset(10, 2),
+        ]),
+
+        segments(exSegmentData1, [
+            $group("y3-x4"),
+            $x(x4), $y(y3),
+            $arcSpread(360), $arcRadius(40), $arcSpanInset(5), $arcRotateBy(30), $radialOffset(10),
         ]),
     ]);
-}, $id("example-segment-2"));
+
+    _.onRenderCompleted(domContainer => {
+        const codeMap = loadCodeMaps();
+        const codePreviewElm = $id("segments-code-preview");
+
+        Object.keys(codeMap).forEach(nodeId => {
+            const node = $id(domContainer.getElementById(nodeId));
+            node.onmouseover = () => {
+                dehighlightNodes(domContainer);
+                node.$removeCSS("in-active");
+                codePreviewElm.innerHTML = codeMap[nodeId]
+            }
+        });
+    });
+}, $id("example-segments"));
+
+function loadCodeMaps() {
+    const codeMap = {};
+
+    codeMap["y1-x1"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x1), $y(y1),</span>
+    <div class='code-focus'>$arcRadius(<span class='active'>40</span>)</div>,
+    <div class='code-focus'>$arcSpanUnit(<span class='active'>90</span>)</div>
+<span class='noise'>]</span>)`
+
+    codeMap["y1-x2"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x2), $y(y1),</span>
+    $arcRadius(<span class='active'>40</span>),
+    $arcSpanUnit(<span class='active'>90</span>),
+    <div class='code-focus'>$arcRadiusOffset(<span class='active'>5</span>)</div>
+<span class='noise'>]</span>)`
+
+    codeMap["y1-x3"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x3), $y(y1),</span>
+    $arcRadius(<span class='active'>40</span>),
+    $arcSpanUnit(<span class='active'>90</span>),
+    $arcRadiusOffset(<span class='active'>5</span>),
+    <div class='code-focus'>$arcRotateBy(<span class='active'>30</span>)</div>
+<span class='noise'>]</span>)`
+
+    codeMap["y1-x4"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x4), $y(y1),</span>
+    $arcRadius(<span class='active'>40</span>),
+    $arcSpanUnit(<span class='active'>90</span>),
+    $arcRadiusOffset(<span class='active'>5</span>),
+    <div class='code-focus'>$radialOffset(<span class='active'>20</span>, <span class='active'>2</span>)</div>
+<span class='noise'>]</span>)`
+
+    codeMap["y2-x1"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x1), $y(y2),</span>
+    $arcRadius(<span class='active'>40</span>),
+    $arcSpanUnit(<span class='active'>90</span>),
+    <div class='code-focus'>$arcSpanOffset(<span class='active'>0</span>)</div>
+<span class='noise'>]</span>)`
+
+    codeMap["y2-x2"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x2), $y(y2),</span>
+    $arcRadius(<span class='active'>40</span>),
+    $arcSpanUnit(<span class='active'>90</span>),
+    <div class='code-focus'>$arcSpanOffset(<span class='active'>10</span>)</div>
+<span class='noise'>]</span>)`
+
+
+    codeMap["y2-x3"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x3), $y(y2),</span>
+    $arcRadius(<span class='active'>40</span>),
+    $arcSpanUnit(<span class='active'>90</span>),
+    $arcSpanOffset(<span class='active'>10</span>),
+    <div class='code-focus'>$arcRotateBy(<span class='active'>30</span>)</div>
+<span class='noise'>]</span>)`
+
+    codeMap["y2-x4"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x4), $y(y2),</span>
+    $arcRadius(<span class='active'>40</span>),
+    $arcSpanUnit(<span class='active'>90</span>),
+    $arcSpanOffset(<span class='active'>10</span>),
+    $arcRotateBy(<span class='active'>30</span>),
+    <div class='code-focus'>$radialOffset(<span class='active'>10</span>, <span class='active'>2</span>)</div>
+<span class='noise'>]</span>)`
+
+    codeMap["y3-x1"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x1), $y(y3),</span>
+    <div class='code-focus'>$arcSpread(<span class='active'>360</span>)</div>,
+    <div class='code-focus'>$arcRadius(<span class='active'>40</span>)</div>
+<span class='noise'>]</span>)`
+
+    codeMap["y3-x2"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x2), $y(y3),</span>
+    $arcSpread(<span class='active'>360</span>),
+    $arcRadius(<span class='active'>40</span>),
+    <div class='code-focus'>$arcSpanInset(<span class='active'>5</span>)</div>
+<span class='noise'>]</span>)`
+
+    codeMap["y3-x3"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x3), $y(y3),</span>
+    $arcSpread(<span class='active'>360</span>),
+    $arcRadius(<span class='active'>40</span>),
+    $arcSpanInset(<span class='active'>5</span>),
+    <div class='code-focus'>$radialOffset(<span class='active'>10</span>, <span class='active'>2</span>)</div>
+<span class='noise'>]</span>)`
+
+    codeMap["y3-x4"] = `segments(<span class='noise'>exSegmentData1, [
+    $x(x4), $y(y3),</span>
+    $arcSpread(<span class='active'>360</span>),
+    $arcRadius(<span class='active'>40</span>),
+    $arcSpanInset(<span class='active'>5</span>),
+    <div class='code-focus'>$arcRotateBy(<span class='active'>30</span>)</div>,
+    <div class='code-focus'>$radialOffset(<span class='active'>10</span>)</div>
+<span class='noise'>]</span>)`
+
+    return codeMap;
+}
+
+function dehighlightNodes(domContainer) {
+    domContainer.childNodes.forEach(node => {
+        const elm = $id(node)
+        elm.$appendCSS("in-active");
+    });
+}
