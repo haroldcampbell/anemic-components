2eb72c83dd0632dd0d8ad242d6b93ec3a8e96f3c
haroldcampbell <harold.campbell@gmail.com>
2020-01-07 22:09:16 +0400

    Update pipeline tests

--- tests/pipeline.test.js
+++ tests/pipeline.test.js
@@ -43,8 +43,8 @@ test("Pipeline", testCase => {
       ])
     ];
 
-    const visualsCallback = _ => {
-      _.addVisuals(visuals)
+    const visualsCallback = domContainer => {
+      domContainer.addVisuals(visuals)
     }
 
     container("test", visualsCallback, parentElement)
@@ -54,7 +54,7 @@ test("Pipeline", testCase => {
 
   testCase.test("container creates svgNode", t => {
     const fixture = setupFixtures();
-    const visualsCallback = _ => {};
+    const visualsCallback = domContainer => {};
 
     container("test", visualsCallback, fixture.parentElement);
 
@@ -69,8 +69,8 @@ test("Pipeline", testCase => {
       empty($data([1, 2, 3]), [])
     ];
 
-    const visualsCallback = _ => {
-      _.addVisuals(visuals)
+    const visualsCallback = domContainer => {
+      domContainer.addVisuals(visuals)
     };
 
     container("test1", visualsCallback);
@@ -78,6 +78,64 @@ test("Pipeline", testCase => {
 
     t.equal(svgNode.tagName, "svg", "should add svgNode to document.body");
     t.true(svgNode, "should add svgNode to document.body when parentElement is not specified");
+    t.end();
+  });
+
+  testCase.test("container calls onRenderCompleted() callback", t => {
+    const data = $data([1])
+    let stateTracker = "";
+
+    const noopCallback = _ => {
+      stateTracker += "a";
+    };
+    const visuals = [
+      empty(data, [$noop(noopCallback), $noop(noopCallback), $noop(noopCallback)])
+    ];
+
+    const emptyCallback = _ => {
+      stateTracker += "b"
+    };
+
+    const visualsCallback = domContainer => {
+      domContainer.addVisuals(visuals)
+      domContainer.onRenderCompleted(emptyCallback)
+    };
+
+    container("test-onRenderCompleted", visualsCallback)
+
+    t.equal(stateTracker, "aaab", "should call onRenderCompleted() after visuals have been rendered");
+    t.end();
+  });
+
+  testCase.test("visual onFinalizeRender() callback called", t => {
+    const data = $data([1])
+    let stateTracker = "";
+
+    const noopCallback = _ => {
+      stateTracker += "a";
+    };
+
+    const visualWithCallback = empty(data, [$noop(noopCallback), $noop(noopCallback), $noop(noopCallback)]);
+    visualWithCallback.onFinalizeRender = () => {
+      stateTracker += "b"
+    }
+
+    const visuals = [
+      empty(data, []),
+      visualWithCallback,
+      empty(data, [$noop(noopCallback), $noop(noopCallback)])
+    ];
+
+    const visualsCallback = domContainer => {
+      domContainer.addVisuals(visuals);
+      domContainer.onRenderCompleted(() => {
+        stateTracker += "x"
+      });
+    };
+
+    container("test-onFinalizeRender", visualsCallback)
+
+    t.equal(stateTracker, "aaabaax", "should call onFinalizeRender() of visual before container onRenderCompleted()");
     t.end();
   });
 });
